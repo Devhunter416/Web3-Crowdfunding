fa011b74169d950b9014d479daa5dea517bcee93
Devhunter416 <seniordev416@gmail.com>
2024-01-22 21:12:34 +0530

    Added Sidebar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from "./App";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
+// import "./styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
 const activeChain = "ethereum";
+import './index.css';
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -17,7 +19,9 @@ root.render(
       clientId={import.meta.env.VITE_TEMPLATE_CLIENT_ID}
       activeChain={activeChain}
     >
+      <Router>
       <App />
+      </Router>
     </ThirdwebProvider>
   </React.StrictMode>
 );
