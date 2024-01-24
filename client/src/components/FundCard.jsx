ae937a82bb4bea2d0e975b3464567f0dafa39f44
Devhunter416 <seniordev416@gmail.com>
2024-01-24 16:15:06 +0530

    Update to profile page

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -7,7 +7,9 @@ const FundCard = ({ owner, title, description, target, deadline, amountCollected
   const remainingDays = daysLeft(deadline);
   
   return (
-    <div className="sm:w-[288px] w-full rounded-[15px] bg-[#1c1c24] cursor-pointer" onClick={handleClick}>
+    <div className="sm:w-[288px] w-full rounded-[15px] bg-[#1c1c24] cursor-pointer" 
+    onClick={handleClick} //this is the function that will be called when the card is clicked, passed from props
+    >
       <img src={image} alt="fund" className="w-full h-[158px] object-cover rounded-[15px]"/>
 
       <div className="flex flex-col p-4">
@@ -15,7 +17,7 @@ const FundCard = ({ owner, title, description, target, deadline, amountCollected
           <img src={tagType} alt="tag" className="w-[17px] h-[17px] object-contain"/>
           <p className="ml-[12px] mt-[2px] font-epilogue font-medium text-[12px] text-[#808191]">Education</p>
         </div>
-
+        {/* Below is just displaying content passed with some nice styles */}
         <div className="block">
           <h3 className="font-epilogue font-semibold text-[16px] text-white text-left leading-[26px] truncate">{title}</h3>
           <p className="mt-[5px] font-epilogue font-normal text-[#808191] text-left leading-[18px] truncate">{description}</p>
--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -56,13 +56,13 @@ export const StateContextProvider = ({ children }) => {
     return parsedCampaings;
   }
 
-//   const getUserCampaigns = async () => {
-//     const allCampaigns = await getCampaigns();
+  const getUserCampaigns = async () => {
+    const allCampaigns = await getCampaigns();
 
-//     const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner === address);
+    const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner === address);
 
-//     return filteredCampaigns;
-//   }
+    return filteredCampaigns;
+  }
 
 //   const donate = async (pId, amount) => {
 //     const data = await contract.call('donateToCampaign', [pId], { value: ethers.utils.parseEther(amount)});
@@ -95,7 +95,7 @@ export const StateContextProvider = ({ children }) => {
         connect,
         createCampaign: publishCampaign,
         getCampaigns,
-        //getUserCampaigns,
+        getUserCampaigns,
         //donate,
         //getDonations
       }}
